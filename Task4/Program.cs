a07f7adf984fa61ca9155574797afe1c39b91612
e.demin <demsq@mail.ru>
2023-02-02 22:16:01 +0300

    Complete  Task 3.4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 
+
+System.Console.WriteLine("Введите число");
+
+int.TryParse(Console.ReadLine(), out int a);
+
+int b = 1;
+
+while(b <= a)
+{
+    System.Console.WriteLine(Math.Pow(b, 2));
+    b++;
+}
